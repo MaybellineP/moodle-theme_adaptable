91a5941fa55df8fc29ea60ca03568a2b2e2a836e
Maybelline Portillo <portillom634@gmail.com>
2025-05-16 08:49:12 -0600

    Update version.php

--- version.php
+++ version.php
@@ -33,7 +33,7 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->component = 'theme_adaptable';
 
 // Adaptable version date (YYYYMMDDrr where rr is the release number).
-$plugin->version = 2024100520;
+$plugin->version = 2024100521;
 
 $plugin->requires = 2024100700.00; // 4.5 (Build: 20241007).
 
