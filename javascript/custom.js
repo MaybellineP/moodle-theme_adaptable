c9727c26ef67cc727f6324d44f92880d93c864d8
Maybelline Portillo <portillom634@gmail.com>
2025-05-25 11:29:22 -0600

    update

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,13 +1,28 @@
 require(["jquery"], function ($) {
-    $(window).on("load", function () {
-       if (["page-user-editadvanced", "page-user-edit"].includes($("body").attr("id"))) {
+  $(document).ready(function () {
+    const pageId = $("body").attr("id");
+    console.log("Current page ID:", pageId);
+
+    if (["page-user-edit", "page-user-editadvanced"].includes(pageId)) {
+      console.log("Edit page detected, loading modules...");
+
+      // Notificar a Moodle que estamos cargando JS (opcional, puedes omitir si no usas "loader" realmente)
       M.util.js_pending("theme_boost/loader");
+
       require(["theme_boost/loader"], function () {
         M.util.js_complete("theme_boost/loader");
       });
-        require(["theme_adaptable/init"], (module) => module.init());
-        require(["theme_adaptable/validateRegistration"], (module) => module.validate());
-      }
-    });
+
+      // Cargar módulos personalizados
+      require(["theme_adaptable/init"], function (module) {
+        console.log("init loaded");
+        module.init();
+      });
+
+      require(["theme_adaptable/validateRegistration"], function (module) {
+        console.log("validate loaded");
+        module.validate();
+      });
+    }
   });
-  
+});
